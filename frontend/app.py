b62bb2e201a3b9cc1ab36f19638ea87f4b8e336c
zyyong27 <156625933+zyyong27@users.noreply.github.com>
2024-04-17 02:36:11 +0800

    feat: create database

---
+++ frontend/app.py
@@ -0,0 +1,23 @@
+import dash 
+from dash import Dash, html, dcc, Input, Output 
+import dash_bootstrap_components as dbc 
+from components import navbar 
+ 
+# Import Open Sans Font for those that is not using it 
+app = Dash(__name__, use_pages=True, external_stylesheets=['https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap', dbc.themes.BOOTSTRAP,'styles.css'],suppress_callback_exceptions=True) 
+ 
+ 
+app.layout = html.Div(children=[ 
+    dcc.Location(id="url"), 
+    navbar,  
+   dbc.Container([ 
+    dash.page_container 
+    ], 
+    id="page-content", 
+    fluid=True 
+   ) 
+], style = {'zoom':'90%'}) 
+ 
+ 
+if __name__ == '__main__': 
+    app.run_server(debug=True)
